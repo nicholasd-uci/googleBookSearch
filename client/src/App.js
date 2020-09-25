72a2c4f97dbe9fdb48fa7e45cd21cfb25e520553
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-09-24 23:44:15 -0700

    included pages import and rendering a router with correct path links

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import Home from './pages/Home'
+import Home from './pages/Saved'
 import { 
   BrowserRouter as Router, 
   Route, 
   Switch, 
   Link
 } from 'react-router-dom'
+import Saved from './pages/Saved'
 
 const App = () => {
   return(
     <Router>
-      
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/saved">Saved</Link>
+        </nav>
+        <Switch>
+          <Router exact path="/" component={Home} />
+          <Router path="/saved" component={Saved} />
+        </Switch>
+      </div>
     </Router>
   )
 }
