615e65406b30cc7c002825366eef20a541bca3f2
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-09-24 22:52:39 -0700

    brought in NPM packages & inclu Proxy script

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 // npm init -y, mongoose, express, axios //
 const express = require('express')
-
+const { join } = require('path')
 const app = express()
 
+// npm i if-env, concurrently --save-dev //
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(join(__dirname, 'client', 'build')))
+}
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(require('./routes'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.get('/*' , (req, res) => {
+        res.sendFile(join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 require('./db')
     .then(() => app.listen(process.env.PORT || 3001))
