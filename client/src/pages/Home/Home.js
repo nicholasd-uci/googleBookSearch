8b7eea86f28513bc1ac0cbc0f6eb3f9c0253c8db
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-09-24 23:55:24 -0700

    brought in useState and rendered UI functionaility

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,8 +1,38 @@
-import React from 'react'
+// need to bring in State Mtg && Functions //
+import React, { useState } from 'react'
 
 const Home = () => {
+
+const [ bookState, setBookState] = useState({
+    search: '',
+    media: []
+})
+
+    bookState.handleInputChange = event => {
+        setBookState({ ...bookState, [event.target.name]: event.target.value })
+    }
+
+    bookState.handleSearchGoogle = event => {
+        event.preventDefault()
+    }
+
     return(
-        <h1>This is the home page</h1>
+        <>
+            <h1>Books Check 'em Out</h1>
+            <h3>Search Books</h3>
+            <form>
+                <p>
+                    <label htmlFor="search">Search</label>
+                    <input type="text" name="search" />
+                </p>
+                <p>
+                    <button onClick={bookState.handleSearchGoogle}>Google Book Search Here</button>
+                </p>
+                <p>
+                    Thanks for using our page <3!
+                </p>
+            </form>
+        </>
     )
 }
 
