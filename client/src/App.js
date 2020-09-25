7ce5179e4125c2bf4ccf5eaaeadac7bedd2323f7
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-09-24 23:24:26 -0700

    added pages folder and included home and saved pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Switch, 
+  Link
+} from 'react-router-dom'
 
 const App = () => {
   return(
-    <h1>Hello World!</h1>
+    <Router>
+      
+    </Router>
   )
 }
