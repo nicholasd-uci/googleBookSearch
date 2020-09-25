e374f0e561dfae9d9c441ad5f2a88fc558f69e93
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-09-24 20:51:31 -0700

    created and tested routes in Postman- successfull

---
+++ routes/bookRoutes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router()
+const { Book } = require('../models')
+
+// GET All
+router.get('/book', (req, res) => {
+    Book.find()
+        .then(book => res.json(book))
+        .catch(err => console.log(err))
+})
+
+
+// POST One
+router.post('/book/', (req, res) => {
+    Book.create(req.body)
+        .then(book => res.json(book))
+        .catch(err => console.log(err))
+})
+
+
+// PUT One
+router.put('/book/:id', (req, res) => {
+    Book.findByIdAndUpdate(req.params.id, req.body)
+    .then(book => res.json(book))
+    .catch(err => console.log(err))
+})
+
+
+// DELETE One
+router.delete('/book/:id', (req, res) => {
+    Book.findById(req.params.id)
+        .then(book => book.remove())
+        .then(book => res.json(book))
+        .catch(err => console.log(err))
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.use(require('./routes'))
+
 require('./db')
     .then(() => app.listen(process.env.PORT || 3001))
     .catch(err => console.log(err))
